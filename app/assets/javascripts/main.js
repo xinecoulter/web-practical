455af1d7347cf8aa0286cb1dfa01eca4fe43eefd
Christine Coulter <christinetcoulter@gmail.com>
2013-10-03 15:53:20 -0400

    add comments

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,9 +3,22 @@ var recommendations = {
     var firstRecommendation = $("<div>").text("helloooooo");
     firstRecommendation.addClass("pop-up");
     $(".container").append(firstRecommendation);
+
+    // needs logic to determine which match the user clicked on--use of id's
     // this would have more information coming from a database (ex. name)
+    // the pop ups need better placement
   }
 };
+
+// script for filling the stats section with information
+// format numbers properly with commas
+// determine if there was a gain or loss in stats in order to classify
+
+// script to add notifications and append to notifications section
+
+// script to remove notifications
+
+// script to show envelope pictures in case of notifications
 
 $(document).ready(function () {
   $(".recommendations").on("click", ".recommendation-summary", recommendations.recommendationPopUp);
